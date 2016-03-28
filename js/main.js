1307a024da3d095431c01e18646b964b43666e9b
Arpine Sargsyan <arpine@bigbek.com>
2016-03-29 00:51:25 +0400

    filter only red divs

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('#red').click(function() {
+    $('.f_red').removeClass('maxHeight');
+    $('.f_red').addClass('minHeight');
+});
+
+$('#all').click(function() {
+
+    $('.f_red').addClass('maxHeight');
+
+});
