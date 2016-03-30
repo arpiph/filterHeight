e23836a58c9460636b81d0257da7b61fd4f37f83
Arpine Sargsyan <arpine@bigbek.com>
2016-03-30 23:58:55 +0400

    all red and green

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 $('#red').click(function() {
+    $('.f_green').removeClass('maxHeight');
     $('.f_red').removeClass('maxHeight');
+
     $('.f_red').addClass('minHeight');
 });
+
+
+$('#green').click(function() {
+    $('.f_red').removeClass('maxHeight');
+    $('.f_green').addClass('minHeight');
+
+});
+
+
+
+
+
 
 $('#all').click(function() {
 
     $('.f_red').addClass('maxHeight');
+    $('.f_green').addClass('maxHeight');
+    $('.f_red').removeClass('minHeight');
+    $('.f_green').removeClass('minHeight');
+
+
 
 });
